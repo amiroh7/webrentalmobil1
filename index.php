58f5e883a0239fd430b1e66c8b21c6bff7e6bb3b
amiroh7 <84594432+amiroh7@users.noreply.github.com>
2021-05-22 03:42:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -244,68 +244,17 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2017 Rental Mobil . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">MH3</a> </p>
 	</div>
 </div>
-<!--- /copy-right ---->
-<!-- sign -->
-			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-						</div>
-							<section>
-								<div class="modal-body modal-spa">
-									<div class="login-grids">
-										<div class="login">
-											<div class="login-right">
-												<form action="prosesdaftar.php" method="post" onsubmit="return validasi_input(this)">
-													<h3>Buat akun Anda</h3>
-													<input type="text" name="nama" placeholder="Masukan Nama lengkap anda">
-													<input type="text" name="username" placeholder="Masukan Username anda">
-													<input type="text" name="alamat" placeholder="Masukan Alamat anda">
-													<input type="text" name="telepon" placeholder="Masukan Telepon anda">
-													<input type="password" name="password" placeholder="Masukan Password anda">	
-													<input type="text" name="noktp" placeholder="Masukan No KTP anda">	
-													<input type="submit" name="daftar" value="CREATE ACCOUNT">
-												</form>
-											</div>
-												<div class="clearfix"></div>								
-										</div>
-										</div>
-								</div>
-							</section>
-					</div>
-				</div>
-			</div>
-
-<!-- //sign -->
-<!-- signin -->
-		<div class="modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content modal-info">
-						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>						
-						</div>
-						<div class="modal-body modal-spa">
-							<div class="login-grids">
-								<div class="login">
-									<div class="login-right">
-										<form action="prosesdaftar.php" method="post" onsubmit="return validasi_input(this)">
-											<h3>Masuk Dengan Akun Anda</h3>
-											<input type="text" name="username" placeholder="Masukan Username anda">
-											<input type="password" name="password" placeholder="Masukan password anda">
-											<input type="submit" value="SIGNIN" name="masuk">
-										</form>
-									</div>
-									<div class="clearfix"></div>								
-								</div>
-								</div>
-						</div>
-					</div>
-				</div>
-			</div>
-<!-- //signin -->
-<!-- write us -->
-<!-- //write us -->
-	</footer>
-</body>
+	<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2017 Rental Mobil . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">MH3</a> </p>
+	</div>
+</div>
 </html>
