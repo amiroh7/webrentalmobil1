dfdbd58f11820465d85574da96d2e0db78a441de
Fajar Handika <amirohadliyah10@gmail.com>
2021-05-22 02:54:21 +0700

    project pertama

---
+++ admin/prosestambahhapus.php
@@ -0,0 +1,25 @@
+<?php 
+include 'proses.php';
+$do = new ClassMobil();
+ ?>
+<?php 
+
+if (isset($_GET['hapusKota'])) {
+	$id = $_GET['hapusKota'];
+	$do->deleteKota($id);
+}else if (isset($_GET['hapusUser'])) {
+	$id = $_GET['hapusUser'];
+	$do->deleteCustomer($id);
+}else if (isset($_GET['hapusHarga'])) {
+	$id = $_GET['hapusHarga'];
+	$do->deleteHarga($id);
+}else if (isset($_GET['hapusMobil'])) {
+	$id = $_GET['hapusMobil'];
+	$do->deleteMobil($id);
+}else if(isset($_GET['hapusPesanan'])){
+	$id = $_GET['hapusPesanan'];
+	$do->deletePesanan($id);
+}
+	
+ ?>
+
